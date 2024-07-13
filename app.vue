0f93da736292ebf18d514c14ec4875c4dd29458b
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2024-07-13 19:55:42 +0100

    feat: create default layout

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <p class="text-xl">hello</p>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="min-h-screen flex justify-center items-center bg-[#F5FAFE]">
+    <div class="w-full max-w-96 rounded-[32px] border border-solid border-gray-300 shadow-[0px_8px_40px_0px_rgba(2,152,252,0.10)] px-4 py-5 flex flex-col gap-y-4">
+      <p class="text-base font-normal self-center">random wallet</p>
+      <slot />
+    </div>
+  </div>
+</template>
